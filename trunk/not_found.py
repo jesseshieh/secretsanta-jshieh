db5da8c80e2116e4336ba02fe83f944b8ab7a97d
jesse.shieh <jesse.shieh@11ebfe08-becc-11dd-871e-457e4afae058>
2008-12-19 15:54:32 +0000

    added a 404, some style, and some extra text

---
+++ trunk/not_found.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# author: Jesse Shieh (jesse.shieh@gmail.com)
+
+def main():
+  print 'Content-Type: text/plain'
+  print ''
+  print 'Not Found'
+
+if __name__ == '__main__':
+  main()
