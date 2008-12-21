4975fa691cbf5f9d1e3651c78d39f9a4a20afa34
jesse.shieh <jesse.shieh@11ebfe08-becc-11dd-871e-457e4afae058>
2008-12-21 05:12:51 +0000

    added a confirm page that simply spits out the post data.  restructured the code a little to make it prettier

--- trunk/main.py
+++ trunk/main.py
@@ -2,23 +2,63 @@
 # author: Jesse Shieh (jesse.shieh@gmail.com)
 
 import os
+import re
 import wsgiref.handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class MainHandler(webapp.RequestHandler):
+class BaseHandler(webapp.RequestHandler):
+  template_values = {
+    "title": "Secret Santa",
+    }
+
+  # adds a new entry in the template dictionary
+  def add_template_value(self, key, value):
+    self.template_values[key] = value
+
+  # special case of add_template_value that pulls the value from the
+  # request object (it's a url parameter of the same name)
+  def add_template_value_from_request(self, key):
+    self.add_template_value(key, self.request.get(key))
+
+  # renders and writes the response given the template name
+  def render(self, template_name):
+    path = os.path.join(os.path.dirname(__file__), template_name)
+    self.response.out.write(template.render(path, self.template_values))
+
+class MainHandler(BaseHandler):
   def get(self):
-    template_values = {
-      'title': 'Secret Santa',
-      }
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
+    self.render("main.html")
+
+class ConfirmHandler(BaseHandler):
+  def post(self):
+    # list of invitees (not including creator)
+    invitees = []
+
+    # invitee post parameter regular expression
+    invitee_re = re.compile(r"invitee(\d+)")
+
+    # iterate through args
+    for key in self.request.arguments():
+      # if this is an invitee, then add it to the invitee array
+      if invitee_re.match(key):
+        invitees.append(self.request.get(key))
+
+    # populate template values
+    self.add_template_value_from_request("creator")
+    self.add_template_value_from_request("price")
+    self.add_template_value("invitees", invitees)
+
+    # render
+    self.render("confirm.html")
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
+  # boilerplate application registration stuff
+  application = webapp.WSGIApplication([("/", MainHandler),
+                                        ("/confirm", ConfirmHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   main()
