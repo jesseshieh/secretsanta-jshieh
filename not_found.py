8773d8c364b49089b0fd92ff2604c51e698a24e0
jesse.shieh <jesse.shieh@11ebfe08-becc-11dd-871e-457e4afae058>
2008-12-22 16:09:35 +0000

    added handling for if emailing returns a non-200.  added a /manage url that doesn't do anything yet

--- not_found.py
+++ not_found.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # author: Jesse Shieh (jesse.shieh@gmail.com)
 
-def main():
-  print 'Content-Type: text/plain'
-  print ''
-  print 'Not Found'
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 
-if __name__ == '__main__':
+class NotFoundPageHandler(webapp.RequestHandler):
+  def get(self):
+    self.error(404)
+    self.response.out.write('Not Found')
+
+def main():
+  application = webapp.WSGIApplication([('/.*', NotFoundPageHandler)],
+                                         debug=True)
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
   main()
